9238c8be6ee981e5adb5e0f088e02d1c440ebc53
Mike North <michael.l.north@gmail.com>
2016-03-19 01:07:25 +0800

    Initial version

---
+++ blueprints/ember-sublime/index.js
@@ -0,0 +1,16 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Setup your Ember project for Sublime Text',
+  normalizeEntityName: function() {}
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
